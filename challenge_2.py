763314f10f8d484fab6ceaa48cf652efd257f774
0siris7 <sarang.prakash2@gmail.com>
2020-02-01 22:56:09 +0530

    Added challenge 2

---
+++ challenge_2.py
@@ -0,0 +1,40 @@
+'''
+WITH DIVISION
+
+a = [int(x) for x in input().split()]
+b = []
+
+def muck(a):
+    tot = 1
+    for i in a:
+        tot *= i 
+
+    return tot 
+
+stuff = muck(a)
+
+for i in a:
+    b.append(stuff // i)
+
+print(b)
+'''
+
+
+#WITHOUT DIVISION
+a = [int(x) for x in input().split()]
+b = []
+
+def muck(a):
+    tot = 1
+    for i in a:
+        tot *= i
+    return tot 
+
+for i in range(len(a)):
+    tem = a.pop(i)
+    k = muck(a)
+    b.append(k)
+    a.insert(i, tem)
+
+print(b)
+
